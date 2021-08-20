b1a7abd0a8badb1ba49dbd73e305b47756596178
Konstantin Minevich <humbl3dev@gmail.com>
2021-08-19 17:07:31 -0700

    add animation to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 import HeroImage from '../public/hero.svg';
 import Wave from '../public/wave.svg';
@@ -13,16 +14,45 @@ const Home: NextPage = () => {
         <div className='container mx-auto max-w-full lg:max-w-7xl'>
           <div className='flex relative'>
             <div>
-              <h1 className='text-4xl font-bold mb-12'>
+              <motion.h1
+                initial={{
+                  y: -10
+                }}
+                animate={{
+                  y: 0
+                }}
+                className='text-4xl font-bold mb-12'>
                 Start your day <br /> with a delicious cup of Doge coffee.
-              </h1>
-              <Link href=''>
-                <a className='py-4 px-12 font-bold rounded-md bg-primary text-white'>See Our Menu</a>
+              </motion.h1>
+              <Link href='/menu' passHref>
+                <motion.a
+                  initial={{
+                    y: 10,
+                    opacity: 0
+                  }}
+                  animate={{
+                    y: 0,
+                    opacity: 1
+                  }}
+                  transition={{
+                    delay: 0.2
+                  }}
+                  className='py-4 px-12 font-bold rounded-md bg-primary text-white inline-block'>
+                  See Our Menu
+                </motion.a>
               </Link>
             </div>
-            <div className='hidden transform sm:block sm:w-72 sm:h-72 sm:ml-14 md:w-auto md:h-auto md:max-w-lg md:block md:static md:-translate-y-28'>
+            <motion.div
+              initial={{
+                opacity: 0
+              }}
+              animate={{ opacity: 1 }}
+              transition={{
+                delay: 0.25
+              }}
+              className='hidden transform sm:block sm:w-72 sm:h-72 sm:ml-14 md:w-auto md:h-auto md:max-w-lg md:block md:static md:-translate-y-28'>
               <Image src={HeroImage} alt='' width={500} height={500} />
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
