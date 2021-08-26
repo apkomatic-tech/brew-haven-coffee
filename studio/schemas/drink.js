0d0a0d4875b6077e062cdfcb5e5bc878004fe496
Konstantin Minevich <humbl3dev@gmail.com>
2021-08-26 16:33:59 -0700

    renaming cms to studio

---
+++ studio/schemas/drink.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'Drink',
+  name: 'drink',
+  type: 'document',
+  // TODO: add icon:
+  // icon: '',
+  fields: [
+    {
+      name: 'drinkname',
+      title: 'Drink Name',
+      type: 'string'
+    }
+  ]
+};
