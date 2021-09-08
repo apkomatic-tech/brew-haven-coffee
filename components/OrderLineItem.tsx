42f816cc63112aa7e36af93c1b74af58cb4fd75b
Konstantin Minevich <humbl3dev@gmail.com>
2021-09-07 17:00:59 -0700

    add css module to order item component

--- components/OrderLineItem.tsx
+++ components/OrderLineItem.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { useContext } from 'react';
 import Image from 'next/image';
 import { useNextSanityImage } from 'next-sanity-image';
+import { HiOutlineTrash as DeleteIcon } from 'react-icons/hi';
 
 import { OrderItem } from '../types/OrderItem';
 import sanityClient from '../sanityClient';
 import CartContext from '../state/cartContext';
+
+import styles from './OrderLineItem.module.css';
 
 function OrderLineItem(item: OrderItem) {
   const { id, title, image, quantity, price } = item;
@@ -13,7 +16,7 @@ function OrderLineItem(item: OrderItem) {
   const { dispatch } = useContext(CartContext);
 
   return (
-    <div className='flex mb-6 pb-6 border-b border-gray-200 relative'>
+    <div className={styles.OrderLineItem}>
       <div>
         <Image {...imageProps} width={167} height={167} alt={title} />
       </div>
@@ -23,7 +26,8 @@ function OrderLineItem(item: OrderItem) {
         </h3>
         <p>Quantity: {quantity}</p>
       </div>
-      <button className='text-primary font-bold absolute bottom-6 right-0' type='button' onClick={() => dispatch({ type: 'REMOVE_ORDER', payload: id })}>
+      <button className={styles.OrderLineItemRemoveBtn} type='button' onClick={() => dispatch({ type: 'REMOVE_ORDER', payload: id })}>
+        <DeleteIcon style={{ marginRight: '0.15rem' }} />
         Remove
       </button>
     </div>
