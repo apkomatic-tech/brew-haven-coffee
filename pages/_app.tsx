269f11b3da023ac6690b0cb0c4bc1888267089f6
Konstantin Minevich <humbl3dev@gmail.com>
2021-10-29 15:46:34 -0700

    add route loading indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
+import { Router } from 'next/router';
 import type { AppProps } from 'next/app';
+import NProgress from 'nprogress';
+
 import Layout from '../components/Layout';
 import { CartProvider } from '../state/cartContext';
 
-import 'react-toastify/dist/ReactToastify.css';
 import '../styles/index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/nprogress-custom.css';
+
+NProgress.configure({
+  showSpinner: false
+});
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+Router.events.on('routeChangeError', () => {
+  NProgress.remove();
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
