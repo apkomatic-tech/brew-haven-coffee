6d721b950001fd0be4bc63d1f6bd7496916adc2b
Konstantin Minevich <humbl3dev@gmail.com>
2022-06-07 21:24:33 -0700

    fixing warnings

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -12,6 +12,8 @@ const Menu: NextPage = (props: any) => {
   function renderDrinks(drinks: any[]) {
     return drinks.map((item: any) => {
       const { _id, name, image, price } = item;
+      console.log(item);
+      console.log(_id);
       const drinkData = {
         id: _id,
         title: name,
@@ -19,7 +21,7 @@ const Menu: NextPage = (props: any) => {
         slug: item.slug.current,
         price
       };
-      return <MenuCard key={_id} {...drinkData} />;
+      return <MenuCard key={drinkData.id} {...drinkData} />;
     });
   }
 
@@ -28,19 +30,19 @@ const Menu: NextPage = (props: any) => {
       <Head>
         <title>Doge Coffee | Menu</title>
       </Head>
-      <div className='page-content wrapper'>
-        <h1 className='page-title'>Menu</h1>
+      <div className="page-content wrapper">
+        <h1 className="page-title">Menu</h1>
 
         {coldDrinks.length > 0 && (
           <>
-            <h2 className='font-bold mb-6 text-xl'>Cold Brews</h2>
-            <div className='grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 mb-12'>{renderDrinks(coldDrinks)}</div>
+            <h2 className="font-bold mb-6 text-xl">Cold Brews</h2>
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 mb-12">{renderDrinks(coldDrinks)}</div>
           </>
         )}
         {hotDrinks.length > 0 && (
           <>
-            <h2 className='font-bold mb-6 text-xl'>Hot Coffee</h2>
-            <div className='grid grid-cols-2 gap-6 sm:grid-cols-3 mb-12'>{renderDrinks(hotDrinks)}</div>
+            <h2 className="font-bold mb-6 text-xl">Hot Coffee</h2>
+            <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 mb-12">{renderDrinks(hotDrinks)}</div>
           </>
         )}
       </div>
@@ -51,7 +53,7 @@ const Menu: NextPage = (props: any) => {
 export const getStaticProps: GetStaticProps = async () => {
   const data = await client.fetch(groq`
     *[_type == 'drink'] {
-      name, price, image, slug, 
+      _id, name, price, image, slug, 
       "category": category->categoryname
     }
   `);
