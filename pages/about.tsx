79f6d2f431410bb34921ae8aea3e9b0d8d6fffbb
Konstantin Minevich <humbl3dev@gmail.com>
2022-08-07 13:48:12 -0700

    more improvements

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import Blob from '../public/yellow-blob.svg';
 
 const About: NextPage = () => {
@@ -24,23 +23,11 @@ const About: NextPage = () => {
         </h1>
         <h2 className='font-bold mb-3 text-xl'>We are here to provide best coffee drinks in the world</h2>
         <p className='text-gray-600 mb-8'>
-          But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound
-          the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is
-          pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or
-          pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
-          To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a
-          man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce with
-          righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid aperiam corporis cum dolorem dolores ducimus ea enim eum ex id illo laborum libero minus modi nam, obcaecati officia optio quasi quos rerum sapiente sequi sit, tempore unde, veniam voluptate! A, aliquam dicta doloremque dolores eveniet fuga magni maxime omnis.
         </p>
         <h2 className='font-bold mb-3 text-xl'>Our Story</h2>
         <p className='text-gray-600'>
-          But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound
-          the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is
-          pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or
-          pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
-          To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a
-          man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce with
-          righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut corporis fuga minima modi molestiae nostrum provident reiciendis sit soluta tempore? Accusantium architecto at aut consequatur culpa ea eligendi error esse eveniet excepturi expedita explicabo fuga id ipsam labore laudantium magnam magni maxime nihil non optio quae quibusdam quis quo rem repellat reprehenderit, sed sint sunt suscipit tempore temporibus tenetur totam. Accusamus ad adipisci architecto dignissimos eveniet in iste magni maiores optio similique. Aut, est fugiat.
         </p>
       </div>
     </>
